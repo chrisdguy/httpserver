3f7d8c0ae07d8e3820c00ebd00df069ba2987162
Chris Guy <chrisdguy@icloud.com>
2025-09-15 03:00:57 -0400

    reads http requests to buffer and prints

--- server.c
+++ server.c
@@ -6,7 +6,9 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define INITIAL_BUFFER_SIZE 1024
 #define MAX_CONNECTIONS 5
+#define PORT 42069
 
 int main() {
 	// Initialize the socket
@@ -20,10 +22,15 @@ int main() {
 	struct sockaddr_in server_addr;
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(42069);
+	server_addr.sin_port = htons(PORT);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	struct sockaddr *server_addr_ptr = (struct sockaddr *)&server_addr;
 	socklen_t server_len = sizeof(server_addr);
+	int yes = 1;
+	if (setsockopt(sock_desc, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) != 0) {
+		perror("Failed to set socket options.");
+		exit(EXIT_FAILURE);
+	}
 	
 	if (bind(sock_desc, server_addr_ptr, server_len) < 0) {
 		perror("Failed to bind.");
@@ -46,6 +53,32 @@ int main() {
 	} else {
 		printf("Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 	}
+	
+	// Reads request to a buffer
+	int buffer_size = INITIAL_BUFFER_SIZE;
+	char *buffer = malloc(INITIAL_BUFFER_SIZE);
+	if (!buffer) {
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	int total_bytes = 0;
+	while (total_bytes < buffer_size - 1) {
+		long bytes_received = recv(client_sock, buffer, buffer_size - 1 - total_bytes, 0);
+		if (bytes_received < 0) {
+			perror("Unable to receive from client");
+			exit(EXIT_FAILURE);
+		} else if (bytes_received == 0) {
+			// connection was closed
+		} else {
+			total_bytes += bytes_received;
+			buffer[total_bytes] = '\0';
+			if (strstr(buffer, "\r\n\r\n")) break;
+		}
+	}
+	printf("Request:\n%s\n", buffer);
+	free(buffer);
+
 	
 	// Clean up
 	close(client_sock);
